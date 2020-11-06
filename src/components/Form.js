36af7d545c5502fefb309dfccd4918869ffc299d
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-06 23:47:40 +0100

    Add usedInForm key to the JSON file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import tableColumns from '../tableColumns.json';
 
-export default function FormData() {
+export default function Form() {
     return (
         <form>Form</form>
     );
