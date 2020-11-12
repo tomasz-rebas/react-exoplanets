fd0b5dfc07ded25b928de518462839b5f3850e78
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-12 23:08:31 +0100

    New component for filtering results

--- src/components/PlanetBrowser.js
+++ src/components/PlanetBrowser.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './Form';
+import PlanetList from './PlanetList';
 
 export default function PlanetBrowser( { planetaryData } ) {
     return (
         <main>
             <Form planetaryData={planetaryData}/>
+            <PlanetList/>
         </main>
     );
 }
