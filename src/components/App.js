b82c14cf6b14ee61ff1c763747ab66c4cd74bb23
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-19 16:10:01 +0100

    Use planet icons

--- src/components/App.js
+++ src/components/App.js
@@ -83,6 +83,22 @@ export default function App() {
             <div className={classes.banana}>
                 <Header isSidebarOpened={isSidebarOpened} setIsSidebarOpened={setIsSidebarOpened}/>
                 <PlanetList planetaryData={planetaryData}/>
+                <footer>
+                    <span>Icons made by </span>
+                    <a 
+                        href="https://www.flaticon.com/authors/freepik"
+                        title="Freepik"
+                    >
+                        Freepik
+                    </a>
+                    <span> from </span>
+                    <a 
+                        href="https://www.flaticon.com/"
+                        title="Flaticon"
+                    >
+                        www.flaticon.com
+                    </a>
+                </footer>
             </div>
         </div> :
         <FetchAlert didFetchFail={didFetchFail}/>
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,14 @@ const useStyles = makeStyles({
     card: {
         width: '200px',
         height: '300px',
-        margin: '10px'
+        margin: '10px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    planetIcon: {
+        width: '100px',
+        height: '100px'
     }
 });
 
@@ -33,6 +40,19 @@ export default function PlanetCard( { data } ) {
             variant="outlined"
             className={classes.card}
         >
+            {
+                pl_dens < 2 ?
+                <img
+                    /*src="https://www.flaticon.com/svg/static/icons/svg/3336/3336008.svg"*/
+                    src="gas-giant.svg"
+                    className={classes.planetIcon}
+                /> :  
+                <img
+                    /*src="https://www.flaticon.com/svg/static/icons/svg/1197/1197992.svg"*/
+                    src="rocky-planet.svg"
+                    className={classes.planetIcon}
+                />
+            }
             {pl_name}
         </Card>
     );
