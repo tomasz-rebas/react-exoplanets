fa83b0f72f78a0cf39f1c31d8849c25f6c0f907d
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-19 15:29:55 +0100

    Add a spinner and styling to the FetchAlert component

--- src/components/FetchAlert.js
+++ src/components/FetchAlert.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    fetchAlert: {
+        textAlign: 'center'
+    }
+});
 
 export default function FetchAlert( { didFetchFail } ) {
+
+    const classes = useStyles();
+    
     return (
-        <div>
+        <div className={classes.fetchAlert}>
             {
                 didFetchFail ?
-                <p>Error occured when fetching data. Please reload and try again.</p> :
-                <p>Fetching data. Please wait...</p>
+                <div>
+                    <p>Error occured when fetching data.</p>
+                    <p>Please reload and try again.</p> 
+                </div> :
+                <div>
+                    <p>Fetching data.</p> 
+                    <p>Please wait...</p>
+                    <CircularProgress/>
+                </div>
             }
         </div>
     );
