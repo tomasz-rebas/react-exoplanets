4d9c03164c58b0ac294f7e2aedb244525ade969f
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-23 20:07:06 +0100

    Implement pagination

--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PlanetCard from './PlanetCard';
 import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
     planetList: {
@@ -15,26 +16,36 @@ const useStyles = makeStyles({
 
 export default function PlanetList( { planetaryData }) {
 
-    const classes = useStyles();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(24);
 
+    const classes = useStyles();
+    const numberOfPages = parseInt(planetaryData.length / itemsPerPage + 1);
     let planets = [];
 
-    for (let i = 0; i < 50; i++) {
-        console.log(planetaryData[i]);
-        planets.push(
-            <PlanetCard 
-                data={planetaryData[i]}
-                key={planetaryData[i].pl_name}
-            />
-        )
+    for (let i = (currentPage - 1) * itemsPerPage; i < currentPage * itemsPerPage; i++) {
+        //console.log(planetaryData[i]);
+        if (i < planetaryData.length) {
+            planets.push(
+                <PlanetCard 
+                    data={planetaryData[i]}
+                    key={planetaryData[i].pl_name}
+                />
+            )
+        }
     }
 
     return (
         <main>
             <div className={classes.planetList}>{planets}</div>
             <Pagination 
-                count={10}
+                count={numberOfPages}
                 color="primary"
+                size="large"
+                onChange={(event, page) => {
+                    setCurrentPage(page);
+                    console.log(planetaryData.length);
+                }}
             />
         </main>
     );
