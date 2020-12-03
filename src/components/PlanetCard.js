d5ee0c7a170c96ff5029f6103cb848e98eec9c33
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-12-03 21:16:17 +0100

    Simplify ternary operator

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -64,21 +64,12 @@ export default function PlanetCard( { data } ) {
             variant="outlined"
             className={classes.card}
         >
-            {
-                pl_dens < 2 ?
-                <img
-                    /*src="https://www.flaticon.com/svg/static/icons/svg/3336/3336008.svg"*/
-                    src="gas-giant.svg"
-                    className={classes.planetIcon}
-                    alt="Planet"
-                /> :  
-                <img
-                    /*src="https://www.flaticon.com/svg/static/icons/svg/1197/1197992.svg"*/
-                    src="rocky-planet.svg"
-                    className={classes.planetIcon}
-                    alt="Planet"
-                />
-            }
+            <img
+                /*src="https://www.flaticon.com/svg/static/icons/svg/1197/1197992.svg"*/
+                src={pl_dens < 2 ? 'gas-giant.svg' : 'rocky-planet.svg'}
+                className={classes.planetIcon}
+                alt="Planet"
+            />
             <h4>{pl_name}</h4>
             {dataForDisplay}
         </Card>
