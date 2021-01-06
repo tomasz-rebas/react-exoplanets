3c26b5cd53c31ed971be0c38aa53b9130a30d2c8
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-01-06 15:40:52 +0100

    Code cleaning

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import tableColumns from '../tableColumns.json';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
-//import Slider from '@material-ui/core/Slider';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Checkbox from '../components/Checkbox';
