9910ed19d2021f712f2ecc309b475635f9ebf5c5
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-09-01 18:42:27 +0200

    Update CORS Proxy Service url

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,90 +1,91 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import getQuery from '../functions/getQuery';
-import convertCsvToObject from '../functions/convertCsvToObject';
-import getUniquePlanets from '../functions/getUniquePlanets';
-import getInitiallyActiveFilters from '../functions/getInitiallyActiveFilters';
+import getQuery from "../functions/getQuery";
+import convertCsvToObject from "../functions/convertCsvToObject";
+import getUniquePlanets from "../functions/getUniquePlanets";
+import getInitiallyActiveFilters from "../functions/getInitiallyActiveFilters";
 
-import tableColumns from '../tableColumns.json';
-import fallbackData from '../fallbackData.json';
+import tableColumns from "../tableColumns.json";
+import fallbackData from "../fallbackData.json";
 
-import Header from './Header';
-import Filters from './Filters';
-import PlanetList from './PlanetList';
-import FetchAlert from './FetchAlert';
-import Footer from './Footer';
+import Header from "./Header";
+import Filters from "./Filters";
+import PlanetList from "./PlanetList";
+import FetchAlert from "./FetchAlert";
+import Footer from "./Footer";
 
-import { Entry } from '../interfaces/Entry';
-import { ActiveFilter } from '../interfaces/ActiveFilter';
+import { Entry } from "../interfaces/Entry";
+import { ActiveFilter } from "../interfaces/ActiveFilter";
 
 export default function App() {
+  const [planetaryData, setPlanetaryData] = useState<Array<Entry>>();
+  const [didFetchFail, setDidFetchFail] = useState<boolean>(false);
+  const [isSidebarOpened, setIsSidebarOpened] = useState<boolean>(false);
+  const [activeFilters, setActiveFilters] = useState<Array<ActiveFilter>>();
 
-    const [planetaryData, setPlanetaryData] = useState<Array<Entry>>();
-    const [didFetchFail, setDidFetchFail] = useState<boolean>(false);
-    const [isSidebarOpened, setIsSidebarOpened] = useState<boolean>(false);
-    const [activeFilters, setActiveFilters] = useState<Array<ActiveFilter>>();
+  // Test only. Will be removed in the final version.
+  const isInDevelopment = false;
 
-    // Test only. Will be removed in the final version.
-    const isInDevelopment = false;
-
-    useEffect(() => {
-        if (isInDevelopment) {
-            setTimeout(() => {
-                setPlanetaryData(fallbackData);
-            }, 1000);
-        } else {
-            fetchData();
-        }
-    }, [isInDevelopment]);
-
-    async function fetchData() {
-
-        const url = `${process.env.REACT_APP_CORS_PROXY}https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=${getQuery(tableColumns, true)}&format=csv`;
-        console.log(`Fetching data from: ${url}`);
-
-        try {
-            const response = await fetch(url);
-            const data = await response.text();
-            const convertedData = convertCsvToObject(data);
-            const strippedData = getUniquePlanets(convertedData);
-            setPlanetaryData(strippedData);
-        } catch (e) {
-            console.error('The error occured. ' + e);
-            setDidFetchFail(true);
-            setTimeout(() => {
-                setPlanetaryData(fallbackData);
-            }, 2000);
-        }
+  useEffect(() => {
+    if (isInDevelopment) {
+      setTimeout(() => {
+        setPlanetaryData(fallbackData);
+      }, 1000);
+    } else {
+      fetchData();
     }
+  }, [isInDevelopment]);
 
-    useEffect(() => {
-        if (planetaryData) {
-            setActiveFilters(
-                getInitiallyActiveFilters(planetaryData, tableColumns)
-            );
-        }
-    }, [planetaryData]);
+  async function fetchData() {
+    const url = `${
+      process.env.REACT_APP_CORS_PROXY
+    }?https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=${getQuery(
+      tableColumns,
+      true
+    )}&format=csv`;
+    console.log(`Fetching data from: ${url}`);
 
-    return (
-        planetaryData && activeFilters ?
-        <div>
-            <Header 
-                isSidebarOpened={isSidebarOpened}
-                setIsSidebarOpened={setIsSidebarOpened}
-            />
-            <Filters
-                isSidebarOpened={isSidebarOpened}
-                setIsSidebarOpened={setIsSidebarOpened}
-                activeFilters={activeFilters}
-                setActiveFilters={setActiveFilters}
-            />
-            <PlanetList 
-                planetaryData={planetaryData}
-                activeFilters={activeFilters}
-                tableColumns={tableColumns}
-            />
-            <Footer/>
-        </div> :
-        <FetchAlert didFetchFail={didFetchFail}/>
-    )
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+      const convertedData = convertCsvToObject(data);
+      const strippedData = getUniquePlanets(convertedData);
+      setPlanetaryData(strippedData);
+    } catch (e) {
+      console.error("The error occured. " + e);
+      setDidFetchFail(true);
+      setTimeout(() => {
+        setPlanetaryData(fallbackData);
+      }, 2000);
+    }
+  }
+
+  useEffect(() => {
+    if (planetaryData) {
+      setActiveFilters(getInitiallyActiveFilters(planetaryData, tableColumns));
+    }
+  }, [planetaryData]);
+
+  return planetaryData && activeFilters ? (
+    <div>
+      <Header
+        isSidebarOpened={isSidebarOpened}
+        setIsSidebarOpened={setIsSidebarOpened}
+      />
+      <Filters
+        isSidebarOpened={isSidebarOpened}
+        setIsSidebarOpened={setIsSidebarOpened}
+        activeFilters={activeFilters}
+        setActiveFilters={setActiveFilters}
+      />
+      <PlanetList
+        planetaryData={planetaryData}
+        activeFilters={activeFilters}
+        tableColumns={tableColumns}
+      />
+      <Footer />
+    </div>
+  ) : (
+    <FetchAlert didFetchFail={didFetchFail} />
+  );
 }
