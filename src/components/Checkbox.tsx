ce48748787ff54434b415e9bdecaf0bc20812011
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2024-11-17 15:42:57 +0100

    Initial commit

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,49 +1,56 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import getFiltersWithUpdatedCheckboxValues from '../functions/getFiltersWithUpdatedCheckboxValues';
-import { ActiveFilter } from '../interfaces/ActiveFilter';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import getFiltersWithUpdatedCheckboxValues from "../functions/getFiltersWithUpdatedCheckboxValues";
+import { ActiveFilter } from "../interfaces/ActiveFilter";
 
 type Props = {
-    value: {
-        name: string,
-        isActive: boolean
-    }, 
-    activeFilter: ActiveFilter,
-    setActiveFilters: Function
-}
+  value: {
+    name: string;
+    isActive: boolean;
+  };
+  activeFilter: ActiveFilter;
+  setActiveFilters: Function;
+};
 
 const useStyles = makeStyles({
-    label: {
-        display: 'block',
-        paddingTop: '5px',
-        paddingBottom: '5px',
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis'
-    }
+  label: {
+    display: "block",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
 });
 
-export default function Checkbox ( { value, activeFilter, setActiveFilters }: Props ) {
+export default function Checkbox({
+  value,
+  activeFilter,
+  setActiveFilters,
+}: Props) {
+  const classes = useStyles();
+  const { name, isActive } = value;
 
-    const classes = useStyles();
-    const { name, isActive } = value;
-    
-    const handleCheckboxChange = (activeFilter: any) => (event: any) => {
-        const checkboxName = event.target.name;
-        setActiveFilters((previousState: ActiveFilter[]) => 
-            getFiltersWithUpdatedCheckboxValues(activeFilter, checkboxName, previousState)
-        );
-    }
-
-    return (
-        <label className={classes.label}>
-            <input 
-                type="checkbox"
-                name={name}
-                checked={isActive}
-                onChange={handleCheckboxChange(activeFilter)}
-            />
-            {name}
-        </label>
+  const handleCheckboxChange = (activeFilter: any) => (event: any) => {
+    const checkboxName = event.target.name;
+    setActiveFilters((previousState: ActiveFilter[]) =>
+      getFiltersWithUpdatedCheckboxValues(
+        activeFilter,
+        checkboxName,
+        previousState
+      )
     );
+  };
+
+  return (
+    <label className={classes.label}>
+      <input
+        type="checkbox"
+        name={name}
+        checked={isActive}
+        onChange={handleCheckboxChange(activeFilter)}
+      />
+      {name}
+    </label>
+  );
 }
--- src/components/CheckboxList.tsx
+++ src/components/CheckboxList.tsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import getToggledTextFilters from '../functions/getToggledTextFilters';
-import { ActiveFilter } from '../interfaces/ActiveFilter';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import getToggledTextFilters from "../functions/getToggledTextFilters";
+import { ActiveFilter } from "../interfaces/ActiveFilter";
 
 type Props = {
-    tableLabel: string,
-    name: string,
-    checkboxes: JSX.Element[],
-    setActiveFilters: Function
-}
+  tableLabel: string;
+  name: string;
+  checkboxes: JSX.Element[];
+  setActiveFilters: Function;
+};
 
 const useStyles = makeStyles({
-    selectAllButton: {
-        marginTop: '15px'
-    },
-    inputContainer: {
-        paddingTop: '10px',
-        paddingBottom: '10px'
-    }
+  selectAllButton: {
+    marginTop: "15px",
+  },
+  inputContainer: {
+    paddingTop: "10px",
+    paddingBottom: "10px",
+  },
 });
 
-export default function CheckboxList ( { tableLabel, name, checkboxes, setActiveFilters }: Props ) {
+export default function CheckboxList({
+  tableLabel,
+  name,
+  checkboxes,
+  setActiveFilters,
+}: Props) {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    const selectAllCheckboxes = () => {
-        setActiveFilters((previousState: ActiveFilter[]) => getToggledTextFilters(previousState, name));
-    }
-
-    return (
-        <div className={classes.inputContainer}>
-            <h4>{tableLabel}</h4>
-            <div>{checkboxes}</div>
-            <Button 
-                variant="contained"
-                className={classes.selectAllButton}
-                onClick={selectAllCheckboxes}
-            >
-                Select All
-            </Button>
-        </div>
+  const selectAllCheckboxes = () => {
+    setActiveFilters((previousState: ActiveFilter[]) =>
+      getToggledTextFilters(previousState, name)
     );
+  };
+
+  return (
+    <div className={classes.inputContainer}>
+      <h4>{tableLabel}</h4>
+      <div>{checkboxes}</div>
+      <Button
+        variant="contained"
+        className={classes.selectAllButton}
+        onClick={selectAllCheckboxes}
+      >
+        Select All
+      </Button>
+    </div>
+  );
 }
