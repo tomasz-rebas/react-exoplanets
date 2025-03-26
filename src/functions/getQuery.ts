ec18587115955d41ad19dd6fd6db449548a8a486
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-03-26 20:52:22 +0100

    getQuery function: use modern JS functions to create columnList

--- src/functions/getQuery.ts
+++ src/functions/getQuery.ts
@@ -1,33 +1,27 @@
-type Column = {
-    databaseColumnName: string
-    usedInQuery: boolean
+interface Column {
+  databaseColumnName: string;
+  usedInQuery: boolean;
 }
 
 export default function getQuery(columns: Column[], noNull: boolean) {
+  const columnList = columns
+    .filter((column) => column.usedInQuery)
+    .map((column) => column.databaseColumnName)
+    .join();
 
-    let columnList = '';
+  let query = `select+distinct+${columnList}+from+ps`;
 
+  if (noNull) {
+    let whereClause = "+where+";
     for (let i = 0; i < columns.length; i++) {
-        if (columns[i].usedInQuery) {
-            columnList += columns[i].databaseColumnName;
-            columnList += ',';
-        }
+      if (columns[i].usedInQuery) {
+        whereClause += columns[i].databaseColumnName + "+is+not+null";
+        whereClause += "+and+";
+      }
     }
-    columnList = columnList.substring(0, columnList.length - 1);
+    whereClause = whereClause.substring(0, whereClause.length - 5);
+    query += whereClause;
+  }
 
-    let query = `select+distinct+${columnList}+from+ps`;
-
-    if (noNull) {
-        let whereClause = '+where+';
-        for (let i = 0; i < columns.length; i++) {
-            if (columns[i].usedInQuery) {
-                whereClause += columns[i].databaseColumnName + '+is+not+null';
-                whereClause += '+and+';
-            }
-        }
-        whereClause = whereClause.substring(0, whereClause.length - 5);
-        query += whereClause;
-    }
-
-    return query;
+  return query;
 }
